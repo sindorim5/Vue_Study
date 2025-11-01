3ee77e1cce8735c3e9192751f5d5c2a855a09d62
Kihun SONG <kihunbuzz@naver.com>
2025-11-01 15:22:58 +0900

    128_End_Of_Cmp_Adv

---
+++ vue-router-composition-api/9_컴포넌트_자세히_알아보기/01_cmp_adv_starting_setup/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <!-- <badge-list></badge-list> -->
+    <!-- <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info> -->
+    <!-- <course-goals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps['another-prop'] }}</p>
+    </course-goals> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+// import BadgeList from './components/UI/BadgeList.vue';
+// import UserInfo from './components/UI/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
+
+export default {
+  components: {
+    TheHeader
+    // , BadgeList
+    // , UserInfo
+    // , CourseGoals
+    , ActiveGoals
+    , ManageGoals
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+      selectedComponent: null
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
