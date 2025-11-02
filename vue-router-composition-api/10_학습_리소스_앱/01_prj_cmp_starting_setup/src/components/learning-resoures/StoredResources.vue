655f672f3b9435b83ada32ecd75a4abb06d467a0
Kihun SONG <kihunbuzz@naver.com>
2025-11-02 14:45:07 +0900

    142_End_Of_Prj_Cmp

---
+++ vue-router-composition-api/10_학습_리소스_앱/01_prj_cmp_starting_setup/src/components/learning-resoures/StoredResources.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="resource in resources"
+      :key="resource.id"
+      :id="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+  inject: ['resources'],
+  components: { LearningResource },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
