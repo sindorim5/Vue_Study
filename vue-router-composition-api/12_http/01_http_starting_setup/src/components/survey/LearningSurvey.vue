c07558b31bf0b47ca06d73ce4a041f32c78711f0
Kihun SONG <kihunbuzz@naver.com>
2025-11-10 15:16:24 +0900

    166_End_Of_Http

--- vue-router-composition-api/12_http/01_http_starting_setup/src/components/survey/LearningSurvey.vue
+++ vue-router-composition-api/12_http/01_http_starting_setup/src/components/survey/LearningSurvey.vue
@@ -9,7 +9,13 @@
         </div>
         <h3>My learning experience was ...</h3>
         <div class="form-control">
-          <input type="radio" id="rating-poor" value="poor" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-poor"
+            value="poor"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-poor">Poor</label>
         </div>
         <div class="form-control">
@@ -23,12 +29,19 @@
           <label for="rating-average">Average</label>
         </div>
         <div class="form-control">
-          <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-great"
+            value="great"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-great">Great</label>
         </div>
-        <p
-          v-if="invalidInput"
-        >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="invalidInput">
+          One or more input fields are invalid. Please check your provided data.
+        </p>
+        <p v-if="error">{{ error }}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -38,12 +51,15 @@
 </template>
 
 <script>
+import constants from '../../constants';
+
 export default {
   data() {
     return {
       enteredName: '',
       chosenRating: null,
       invalidInput: false,
+      error: null,
     };
   },
   emits: ['survey-submit'],
@@ -59,6 +75,30 @@ export default {
         userName: this.enteredName,
         rating: this.chosenRating,
       });
+
+      this.error = null;
+      fetch(constants.FIREBASE_URL + 'surveys.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: {
+          name: this.enteredName,
+          rating: this.chosenRating,
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            ///
+          } else {
+            console.log('1 ' + response);
+            throw new Error('Could not save survey data.');
+          }
+        })
+        .catch((err) => {
+          console.log('2 ' + err);
+          this.error = err.message;
+        });
 
       this.enteredName = '';
       this.chosenRating = null;
--- vue-router-composition-api/12_http/01_http_starting_setup/src/components/survey/UserExperiences.vue
+++ vue-router-composition-api/12_http/01_http_starting_setup/src/components/survey/UserExperiences.vue
@@ -3,9 +3,16 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && !error && (!results || results.length === 0)">
+        No stored experiences found.
+      </p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -19,11 +26,53 @@
 
 <script>
 import SurveyResult from './SurveyResult.vue';
+import constants from '../../constants';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null,
+    };
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      this.error = null;
+      fetch(constants.FIREBASE_URL + 'surveys.json', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        })
+        .catch((err) => {
+          this.error = err.message;
+          this.isLoading = false;
+        });
+    },
+  },
+  mounted() {
+    this.loadExperiences();
   },
 };
 </script>
