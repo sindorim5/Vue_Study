23aca1c1a680c979a8c2fefe8de39ead5f37e9c2
Kihun SONG <kihunbuzz@naver.com>
2025-11-11 17:47:00 +0900

    177_Navigation_And_Dynamic_Paths

--- vue-router-composition-api/13_Routing/01_routing_starting_setup/src/main.js
+++ vue-router-composition-api/13_Routing/01_routing_starting_setup/src/main.js
@@ -1,7 +1,24 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
-const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UsersList },
+    // { path: 'teams/new' } teamId가 new인 도메인으로 인식 될 수 있으므로 :teamId는 나중에
+    { path: '/teams/:teamId', component: TeamMembers },
+  ],
+  linkActiveClass: 'activeLink',
+});
+
+const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
